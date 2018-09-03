81bc3f13eb1d932339de8ffdf4d5fa341d1cf7b7
DzianisH <gavrilovetsden@gmail.com>
2018-09-03 23:07:54 +0300

    Add example for DC1

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <MotorDriver.h>
 
+
+const int myDelay = 1500;
+MotorDriver m;
+
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("hello world");
 
+  m.motor(1,FORWARD,255);
+  delay(myDelay);
+  m.motor(1,FORWARD,125);
+  delay(myDelay);
+  m.motor(1,RELEASE,0);
+  delay(myDelay);
+  m.motor(1,BACKWARD,125); 
+  delay(myDelay);
+  m.motor(1,BACKWARD,255); 
+  delay(myDelay);
+  m.motor(1,BACKWARD,125); 
+  delay(myDelay);
+  m.motor(1,RELEASE,0);
+  delay(myDelay);
 }
